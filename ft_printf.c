2f6998f2f7bd2d813a603af916b7eb395175e8b4
Curtis Krommenhoek <ckrommen@e1z3r3p23.42.us.org>
2017-12-19 17:28:05 -0800

    printf progress

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ckrommen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/18 16:30:54 by ckrommen          #+#    #+#             */
-/*   Updated: 2017/12/19 15:01:57 by ckrommen         ###   ########.fr       */
+/*   Updated: 2017/12/19 17:26:50 by ckrommen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,36 +18,74 @@ char *ft_remalloc(char *str)
 
 	if (str)
 	{
+		printf("%zu\n", ft_strlen(str));
 		new = ft_strnew(ft_strlen(str) + 1);
-		ft_memset((void *)new, '.', ft_strlen(str) + 1)
+		ft_memset((void *)new, '.', ft_strlen(str) + 1);
 		new = ft_strcpy(new, str);
-		free(str);
+		ft_strdel(&str);
 		return (new);
 	}
 	return (NULL);
 }
 
-
-
-char	*parse_format(char *format, char *res, va_list ap)
+char	*parse_flags(char *flag, va_list ap)
 {
-	res = ft_strnew(1);
+	if (*flag == 's')
+		return (va_arg(ap, char *));
+	else if (*flag == 'S')
+		return ("BROKEN");
+	else if (*flag == 'p')
+		return ("MEMORY");
+	else if (*flag == 'd')
+		return (ft_itoa(va_arg(ap, int)));
+	else if (*flag == 'D')
+		return ("LONG_INT");
+	else if (*flag == 'i')
+		return (ft_itoa(va_arg(ap, int)));
+	else if (*flag == 'o')
+		return (ft_itoa(va_arg(ap, int)));
+	else if (*flag == 'O')
+		return ("LONG_INT");
+	else if (*flag == 'u')
+		return ("UNSIGNED_INT");
+	else if (*flag == 'U')
+		return ("UNSIGNED_INT");
+	else if (*flag == 'x')
+		return (ft_itoa(va_arg(ap, int)));
+	else if (*flag == 'X')
+		return (ft_itoa(va_arg(ap, int)));
+	else if (*flag == 'c')
+		return (va_arg(ap, char *));
+	else if (*flag == 'C')
+		return ("BROKEN");
+	else
+		return ("END");
+}
+
+void	parse_format(char *format, char **res, va_list ap)
+{
+	int i;
+
+	i = 0;
 	while (*format)
 	{
-		if (is_alpha(*format))
+		if (ft_isalpha(*format) || *format == 32)
 		{
 			if (!*res)
-				res = ft_remalloc(res);
-			*res = *format;
-			res++;
+				*res = ft_strnew(1);
+			else if (!*res[i])
+				*res = ft_remalloc(*res);
+			*res[i] = *format;
+			i++;
 		}
-		else if (!is_alpha(*format))
+		else if (*format == '%')
 		{
-			parse_flags(format);
+			format++;
+			*res = ft_strjoin(*res, parse_flags(format, ap));
+			i = ft_strlen(*res);
 		}
 		format++;
 	}
-	return (res);
 }
 
 int ft_printf(char const *restrict format, ...)
@@ -56,16 +94,14 @@ int ft_printf(char const *restrict format, ...)
 	char *res;
 
 	va_start(ap, format);
-	parse_format((char *)format, res, ap);
-	
+	parse_format((char *)format, &res, ap);
+	ft_putstr(res);
 	return (1);
 }
 
 int main(void)
-{
-
-	
-	ft_printf("%i %i %c %s", 9, 8, 'c', "string");
+{	
+	ft_printf("as%i%s", 1, "string");
 	return 0;
 }
 
